79742db1f6b99d11e8fb3df40f8a1ae188828208
Unknown <paedar.dev@gmail.com>
2018-03-04 22:45:32 +0100

    Added a replayfile
    This is a replay file in which I score a sweet game-winner in OT. But that's not why it is here. It has a new enough version to have a net version. It also contains names which require a different character set to be able to read.

--- src/test/java/rocketleague/replays/parser/ReplayParserTest.java
+++ src/test/java/rocketleague/replays/parser/ReplayParserTest.java
@@ -7,7 +7,7 @@ import rocketleague.replays.parser.util.FileLoader;
 public class ReplayParserTest {
 	@Test
 	public void testReader() {
-		String replayFile = "1.11.replay";
+		String replayFile = "A28DA8C44B925B14DBD73592885A9507.replay";
 		ReplayParser parser = new ReplayParser(FileLoader.getFileData(replayFile));
 
 		parser.parse();
